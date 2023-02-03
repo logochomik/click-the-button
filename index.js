7fc2f3e640f7ca6b28d82caa8f3fd1ff52e1d53f
logochomik <findthechomiks@gmail.com>
2023-02-03 14:31:26 -0500

    cursors

--- index.js
+++ index.js
@@ -6,3 +6,21 @@ function getMoney() {
     num += 1 * multiplier;
     money.innerHTML = num;
 }
+
+function buyCursor() {
+    let money = document.getElementById('money');
+    let num = parseInt(money.textContent);
+    let multNotice = parseInt(document.getElementById('multiplier').textContent);
+    let cursors = parseInt(document.getElementById('cursors').textContent);
+
+    if (num < 10) {
+        alert('You do not have enough money to buy a cursor.')
+    }
+    else {
+        num -= 10;
+        money.innerHTML = num;
+        multiplier += 0.1;
+        multNotice += 0.1;
+        cursors += 1;
+    }
+}
